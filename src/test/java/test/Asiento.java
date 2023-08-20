aaf04a56750359499ba8c69723fa0714fe089abd
Lualmara-5 <luimartinezra@unal.edu.co>
2023-08-20 15:58:34 -0500

    Clase Asiento Part 2

--- src/test/java/test/Asiento.java
+++ src/test/java/test/Asiento.java
@@ -7,6 +7,7 @@ public class Asiento {
 	
 	void cambiarColor(String color){
 		if (color == "rojo" || color == "verde" || color == "amarillo" || color == "negro" || color == "blanco"){
+			this.color = color;
 		}
 	}
 }
