23d6c768f054f9be037a2b901e3d0ca39a1db928
Lualmara-5 <luimartinezra@unal.edu.co>
2023-08-20 14:29:49 -0500

    Clase Motor Part 2

--- src/test/java/test/Motor.java
+++ src/test/java/test/Motor.java
@@ -3,5 +3,10 @@ package test;
 public class Motor {
 	
 	int Ncilindros;
-	
+	String tipo;
+    int registro;
+    
+    void cambiarRegistro (int registro){
+    	this.registro = registro;
+    }
 }
